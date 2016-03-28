d2f86b7eaaf1f0f5b90e14d554af9f62cf1a82b3
hcape <hcape@uvic.ca>
2016-03-28 14:57:38 -0700

    Add list of lockers based on status. Add conditional home page based on locker avalibility

--- Lockers/public/core.js
+++ Lockers/public/core.js
@@ -1,9 +1,13 @@
 var app=angular.module('lockers',[]);
+
 app.controller('lockers',function($scope,$http,$interval){
-load();
-function load(){
-$http.get('http://localhost:8000/load').success(function(data){
-$scope.users=data;
-});
-};
+    load();
+    function load(){
+        $http.get('http://localhost:8000/load').success(function(data){
+            $scope.users=data;
+            $scope.open = $scope.users.filter(function(user){
+                return user.status ==='open';
+            });
+        });
+    };
 });
